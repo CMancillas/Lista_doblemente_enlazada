5fdeeec839aca733a65f027fc046ed94cb4abb48
CMancillas <125095040+CMancillas@users.noreply.github.com>
2024-03-18 18:35:44 -0700

    Agregue archivos nuevos al repositorio

---
+++ ListasDobles.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Carlos Mancillas on 2024-03-15.
+//
+
+#include "ListasDobles.h"
---
+++ ListasDobles.h
@@ -0,0 +1,22 @@
+
+
+#ifndef LISTASDOBLEMENTEENLAZADAS_LISTASDOBLES_H
+#define LISTASDOBLEMENTEENLAZADAS_LISTASDOBLES_H
+
+
+class ListaDoble{
+public:
+
+    // Constructor
+    ListaDoble();
+    // Destructor
+    ~ListaDoble();
+    // Construtor de copias
+    ListaDoble(const ListaDoble & lista);
+    // Operador de asignacion
+    ListaDoble & operator=(const ListaDoble & lista);
+
+
+};
+
+#endif //LISTASDOBLEMENTEENLAZADAS_LISTASDOBLES_H
